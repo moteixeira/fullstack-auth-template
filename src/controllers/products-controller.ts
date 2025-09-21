470416387ce7204aec644cf8728a1ee8195068b4
Marcos Gabriel <moliveirateixeira96@gmail.com>
2025-09-21 13:48:52 +0100

    Adding roles and products

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -3,7 +3,10 @@ import { Request, Response } from "express"
 class ProductsController {
   async index(request: Request, response: Response) {}
 
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+
+    return response.json({message: request.user?.role})
+  }
 }
 
 export { ProductsController }
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express"
 import { ProductsController } from "@/controllers/products-controller"
+import { ensureAuthenticated } from "@/middleware/ensureAuthenticated"
+import { verifyUserAuthorization } from "@/middleware/verifyUserAuthorization"
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+// Aplicar em todas as rotas:
+// productsRoutes.use(verifyUserAuthorization(["sale"]))
+
+
 productsRoutes.get("/", productsController.index)
-productsRoutes.post("/", productsController.create)
+productsRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["sale"]), productsController.create)
 
 export { productsRoutes }
